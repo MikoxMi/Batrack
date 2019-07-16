7f03a6386854b48859c2ac7ce2677f99f13632e1
MikoxMi <mrx22661@gmail.com>
2019-07-16 20:34:41 +0400

    Add channels art to list

--- cogs/event.py
+++ cogs/event.py
@@ -11,7 +11,7 @@ class Events(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.Cog.listener()
+
     async def on_ready(self):
         """
         Instructions after authorization
@@ -89,6 +89,7 @@ class Events(commands.Cog):
         member = discord.utils.get(guild.members, id=user_id)
 
         if more_roles is not True:
+            print('true')
             for role_check in member.roles:
                 for role_record in rec_roles:
                     if str(role_check) == str(role_record):
@@ -136,6 +137,7 @@ class Events(commands.Cog):
         guild = discord.utils.get(self.bot.guilds, id=guild_id)
         role = discord.utils.get(guild.roles, name=rec_roles[_id])
         member = discord.utils.get(guild.members, id=user_id)
+        print('true')
         await member.remove_roles(role)
 
     @commands.Cog.listener()
@@ -230,7 +232,7 @@ class Events(commands.Cog):
         await Mongo.delete_record('members', 'id', member.id)
         await Mongo.delete_record('members', 'id', member.id)
 
-    @commands.Cog.listener()
+
     async def on_command_error(self, ctx, error):
         """Реакция на ошибки в командах или Cooldown"""
         if isinstance(error, commands.NoPrivateMessage):
