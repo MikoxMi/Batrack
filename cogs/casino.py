8d55ba70ca8796c76acb1937931580459918c738
MikoxMi <mrx22661@gmail.com>
2019-07-18 22:47:12 +0400

    Fix old casino to new

--- cogs/casino.py
+++ cogs/casino.py
@@ -20,18 +20,17 @@ class Casino(commands.Cog):
         
         -br <bet>"""
 
-        emoji = await DiscordUtils.get_emoj(ctx)
+        emoji = await DiscordUtils.get_emoji(ctx)
 
         if not bet.isdigit():
             await ctx.send("Используйте только числа")
             return
 
         #Get money
-        record = self.members.find_one({"user_id":ctx.message.author.id})
         record = await Mongo.get_record('members', 'id', ctx.message.author.id)
         money = int(record['money'])
         if money < int(bet):
-            await self.bot.say('Недостаточно денег для игры')
+            await ctx.send('Недостаточно денег для игры')
             return
         
         count = money - int(bet)
@@ -43,17 +42,17 @@ class Casino(commands.Cog):
         # Game
         ball = random.randint(0, 100)
         if ball >= 0 and ball <= 60:
-            await self.bot.say(f"{ctx.message.author.mention}. Вы проиграли. Выпало число {ball}")
+            await ctx.send(f"{ctx.message.author.mention}. Вы проиграли. Выпало число {ball}")
             return
         elif ball >= 60 and ball <= 89:
             networth = int(bet) * 2
-            await self.bot.say(f"{ctx.message.author.mention}. Вы выйграли: {networth}{emoji}. Умножение: x2")
+            await ctx.send(f"{ctx.message.author.mention}. Вы выйграли: {networth}{emoji}. Умножение: x2")
         elif ball >= 90 and ball <= 99:
             networth = int(bet) * 4
-            await self.bot.say(f"{ctx.message.author.mention}. Вы выйграли: {networth}{emoji}. Умножение: x4")
+            await ctx.send(f"{ctx.message.author.mention}. Вы выйграли: {networth}{emoji}. Умножение: x4")
         else:
             networth = int(bet) * 14
-            await self.bot.say(f"{ctx.message.author.mention}. Вы выйграли: {networth}{emoji}. Умноежение: x14")
+            await ctx.send(f"{ctx.message.author.mention}. Вы выйграли: {networth}{emoji}. Умноежение: x14")
 
         count = money + networth
         upg = {
